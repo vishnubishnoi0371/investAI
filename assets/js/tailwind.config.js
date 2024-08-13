ba51e3240b2edd511e818de56ce3c8ab307d8670
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-08-13 12:19:11 +0530

    first commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'light-blue': '#00BFFF',
+                'blue': '#00A4FF',
+                'green': '#01D61B',
+                'light-black': '#29333A',
+            },
+            fontSize: {
+                'custom-md': '19px',
+                'custom-lg': '28px',
+                'custom-5xl': '50px',
+            },
+            fontFamily: {
+                'roboto': ['Roboto', 'sans-serif'],
+                'oxygen': ['Oxygen', 'sans-serif'],
+                'oxanium': ['Oxanium', 'sans-serif'],
+            },
+            lineHeight: {
+                '117': '117%',
+                '120': '120%',
+                '127': '127%',
+                '150': '150%',
+            }
+        }
+    },
+  };
+  
+
